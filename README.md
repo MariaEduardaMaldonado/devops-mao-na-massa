832c851499d842720d4bce52da23759f71dad12e
MariaEduardaMaldonado <madulm75@gmail.com>
2022-10-26 12:10:15 -0300

    ansible-lab

--- README.md
+++
@@ -1,1 +0,0 @@
-# devops-mao-na-massa
---
+++ ansible-lab/app01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cat << EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDU4ur3vftmn44xBd8f6J6BvST66Pb7wkb/AymRoKCrmGHYwnj5FxOScrFpNROJWn1nP2/eUb+N9qtC103df11MzQGVrjsMAjjuAxjfUarnlvPohE+3EJq/QhW2FFGx5uAukEKiRnTWxNECg1+S0B8vG3UnLoWbllVeopxkj8C1EjvWOS4k2scGqJU4zN9O8Z2C9fYib55MPMSX/iooRYHYz6Ylp4jgM+lEtIGJZvI9nce/2Nk4yOKye2uCVzteoAFeB2oQRUhPuVX1Iih03/dZbqip9GG2tkMKJ6uGgCr7Qs+TNIagtG2Gd41E45R+ZG2GfhvQmGZv7fkbPQ9YocO1 vagrant@control-node
+EOT
